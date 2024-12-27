69d404e9d39bd6bc4d8f1ad7167cf83ea03ec595
Ravindra Bijarniya <ravindrabijarniya123@gmail.com>
2024-12-27 10:20:15 +0530

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Header.css";
+import logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+
+const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  return (
+    <div className="header">
+      <img src={logo} alt="" className="logo" />
+
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li>Home</li>
+          <li>Programs</li>
+          <li>Why us</li>
+          <li>Plans</li>
+          <li>Testimonials</li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Header;
